b3569a5547fdf49207352ebcb4945af13e3c11ba
Andywarrior07 <andywarrior0707@gmail.com>
2015-04-28 13:04:32 -0400

    farmaacias y productos
    Es el insert de las farmacias con sus productos y su direccion. 
    Hay que agregar los productos

---
+++ insert.js
@@ -0,0 +1,99 @@
+db.farmacias.insert(
+[
+	{
+		direccion:"San Martin",
+		numero:"174",
+		comuna:"Buin",
+		productos:[]
+	},
+	{
+		direccion:"Bernardino Bravo",
+		numero:"15",
+		comuna:"Buin",
+		productos:[]
+	},
+	{
+		direccion:"Calle Jose Manuel Balmaceda",
+		numero:"124",
+		comuna:"Buin",
+		productos:[]
+	},
+	{
+		direccion:"Av. Americo Vespucio",
+		numero:"2500",
+		comuna:"Cerrillos",
+		productos:[]
+	},
+	{
+		direccion:"Av. Americo Vespucio",
+		numero:"1501",
+		comuna:"Cerrillos",
+		productos:[]
+	},
+	{
+		direccion:"Carretera Gral. San Martin",
+		numero:"68",
+		comuna:"Colina",
+		productos:[]
+	},
+	{
+		direccion:"Caletera Gral. San Martin",
+		numero:"19000",
+		comuna:"Colina",
+		productos:[]
+	},
+	{
+		direccion:"Calle Paseo Colina Sur",
+		numero:"14500",
+		comuna:"Colina",
+		productos:[]
+	},
+	{
+		direccion:"Chicureo",
+		numero:"130",
+		comuna:"Colina",
+		productos:[]
+	},
+	{
+		direccion:"Pedro Fontova",
+		numero:"5801",
+		comuna:"Conchali",
+		productos:[]
+	},
+	{
+		direccion:"Independencia",
+		numero:"4142",
+		comuna:"Conchali",
+		productos:[]
+	},
+	{
+		direccion:"Riquelme",
+		numero:"69",
+		comuna:"El Bosque",
+		productos:[]
+	},
+	{
+		direccion:"AV. LIB. B. OHIGGINS",
+		numero:"4103",
+		comuna:"Estacion Central",
+		productos:[]
+	},
+	{
+		direccion:"Av. Gral VElasquez",
+		numero:"60",
+		comuna:"Estacion Central",
+		productos:[]
+	},
+	{
+		direccion:"Av. Libertador Bdo. O'Higgins",
+		numero:"5199",
+		comuna:"Estacion Central",
+		productos:[]
+	},
+	{
+		direccion:"Exposicion",
+		numero:"51",
+		comuna:"Estacion Central",
+		productos:[]
+	}
+])
