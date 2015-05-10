2e88d55c38bdb5fcc00d54043832da048d901d14
Andywarrior07 <andywarrior0707@gmail.com>
2015-05-10 19:58:56 -0400

    dasdasd

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+if [ "$BRANCH" != "master" ]
+
+   then
+   echo "Only master will be deployed. No deployment for this branch: $BRANCH"
+
+else
+
+   echo "deploying master branch"}
+   echo "deployed successfully"
+
+fi
