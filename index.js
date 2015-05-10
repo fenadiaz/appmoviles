6ea86b33ed0ab2bddaca1f0b92b90d163f65b8c6
Andywarrior07 <andywarrior0707@gmail.com>
2015-05-10 02:32:20 -0400

    d

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ var logger = require('./lib/logger');
 
 var options, app;
 
-/*
- * Create and configure application. Also exports application instance for use by tests.
- * See https://github.com/krakenjs/kraken-js#options for additional configuration options.
- */
 options = {
     onconfig: function (config, next) {
 
