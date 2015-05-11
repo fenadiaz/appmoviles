a631f0a0e0eac668164a3d916a09ceb34d5d31f5
Andywarrior07 <andywarrior0707@gmail.com>
2015-05-10 20:59:01 -0400

    tyuty

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.on('start', function () {
 
 if(!module.parent){
     var server = http.createServer(app);
-    var ipaddress = "127.0.0.1";
-    var port = 8000;
+    var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+    var port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
     logger.info('ipaddress: ' + ipaddress);
     logger.info('port: ' + port);
     server.listen( port, ipaddress, function() {
