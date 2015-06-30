0c45192cb02923c8583667d80122e6a4ad0a13fd
Andywarrior07 <andywarrior0707@gmail.com>
2015-06-30 00:39:21 -0400

    dewrewrw

--- controllers/api/login/index.js
+++ controllers/api/login/index.js
@@ -1,31 +1,33 @@
 'use strict';
 
-var userlib = require('../../../lib/userLib');
+var usersLib = require('../../../lib/usersLib');
 
 module.exports = function(router){
 
-    router.get('/',function(req,res){
+    router.get('/', function (req,res){
       res.status(200).end();
     });
 
-    router.post('/',function(req,res){
-      console.log('post login');
-      var data = req.body;
-      console.log(data);
-      var username = data.username;
-      var password = data.password;
+     router.post('/', function (req, res) {
 
-      userlib.findOne(username,password,function(error,user){
-        if(error && error.message === 'NOT_FOUND'){
-          return res.status(401).json({message:'USER_OR_PASSWORD_NOT_FOUND'});
-        }
-        if(error){
-          return res.status(500).json(error).end();
-        }//sdf
-        var secret = 'secretpass';
-        userlib.getToken(user, secret, function(error,token){
-          return res.status(201).json({'token':token}).end();
+        var userData = req.body;    
+
+        usersLib.findOne(userData.username, userData.password, function(error, user){
+
+            if (error && error.message === 'NOT_FOUND'){
+                return res.status(401).json({message: 'USER_OR_PASSWORD_NOT_FOUND'}).end();
+            }
+
+            if (error){
+                return res.status(500).json(error).end();
+            }
+            console.log(user);
+            usersLib.getToken(user.id, function(error, token){
+                return res.status(201).json({'token': token}).end();  
+            });      
+
         });
-      });
+
     });
+
 };
